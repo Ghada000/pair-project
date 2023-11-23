6e11cc0695369a63df4568582cc6e6ad504570ff
JasserGa <jessergafsi2@gmail.com>
2023-11-23 12:56:10 +0300

    ...

--- sign up/main.js
+++ sign up/main.js
@@ -2,3 +2,7 @@ console.log($)
 $(".btn2").click(function(){
   window.location.href= 'https://www.bing.com/images/search?q=taking+houses+by+force+in+gaza&qpvt=taking+houses+by+force+in+gaza&form=IGRE&first=1'; 
 })
+function showMessage() {
+  alert("Your account is created. Sign in now.");
+  // You can redirect the user to the sign-in page here if needed.
+}
